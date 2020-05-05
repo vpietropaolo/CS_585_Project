0ecff000b9933b78c871a49d7ffb4a970b5ec091
harsh-malara <harsh-malara@github.com>
2020-05-04 20:47:55 -0400

    Harsh timer and health bar

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d975b0983ec5688ae95bb46695acc06fac57e79807b193db51b14fe07957dad5
-size 4886
+oid sha256:8d9c9108a63baac753c087513937b8eb0a5b20e1c425c6c5c5b7d5787e0316e6
+size 6907
