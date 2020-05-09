984a9216b740460357e98296cab41a8e270c350d
harsh-malara <harsh-malara@github.com>
2020-05-09 11:16:55 -0400

    fire breath reduce life

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1f67b3eed0acfe99ebdc68095fd6ed0e649b96b97a6be9d1dbc3a0c164c5919
-size 3519
+oid sha256:57f6c86583dd007bea5cfee893eb2f52de854fe84a9ff2c5bcee9f11a880e5ad
+size 3737
